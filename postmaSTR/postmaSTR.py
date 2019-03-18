ba28714b916eadea274cb79ee552db9017df4599
Nima Mousavi <mousavi@ucsd.edu>
2019-03-18 14:55:44 -0700

    Added min and max prob to input options

--- postmaSTR/postmaSTR.py
+++ postmaSTR/postmaSTR.py
@@ -35,24 +35,60 @@ def CheckFilters(invcf, args):
     - args (argparse namespace)
     Exit program if checks fail
     """
-    if args.affec_expansion_prob_het is not None:
-        if args.affec_expansion_prob_het < 0 or args.affec_expansion_prob_het > 1:
-            common.ERROR("--affec-expansion-prob-het must be between 0 and 1")
-    if args.unaff_expansion_prob_het is not None:
-        if args.unaff_expansion_prob_het < 0 or args.unaff_expansion_prob_het > 1:
-            common.ERROR("--unaff-expansion-prob-het must be between 0 and 1")
-    if args.affec_expansion_prob_hom is not None:
-        if args.affec_expansion_prob_hom < 0 or args.affec_expansion_prob_hom > 1:
-            common.ERROR("--affec_expansion-prob-hom must be between 0 and 1")
-    if args.unaff_expansion_prob_hom is not None:
-        if args.unaff_expansion_prob_hom < 0 or args.unaff_expansion_prob_hom > 1:
-            common.ERROR("--unaff_expansion-prob-hom must be between 0 and 1")
-    if args.affec_expansion_prob_total is not None:
-        if args.affec_expansion_prob_total < 0 or args.affec_expansion_prob_total > 1:
-            common.ERROR("--affec_expansion-prob-total must be between 0 and 1")
-    if args.unaff_expansion_prob_total is not None:
-        if args.unaff_expansion_prob_total < 0 or args.unaff_expansion_prob_total > 1:
-            common.ERROR("--unaff_expansion-prob-total must be between 0 and 1")
+    if args.affec_max_expansion_prob_het is not None:
+        if args.affec_max_expansion_prob_het < 0 or args.affec_max_expansion_prob_het > 1:
+            common.ERROR("--affec-max-expansion-prob-het must be between 0 and 1")
+    if args.affec_min_expansion_prob_het is not None:
+        if args.affec_min_expansion_prob_het < 0 or args.affec_min_expansion_prob_het > 1:
+            common.ERROR("--affec-min-expansion-prob-het must be between 0 and 1")
+    if args.affec_min_expansion_prob_het is not None and args.affec_max_expansion_prob_het is not None:
+        if args.affec_min_expansion_prob_het > args.affec_max_expansion_prob_het:
+            common.ERROR("--affec-min-expansion-prob-het must be less than --affec-max-expansion-prob-het")
+    if args.unaff_max_expansion_prob_het is not None:
+        if args.unaff_max_expansion_prob_het < 0 or args.unaff_max_expansion_prob_het > 1:
+            common.ERROR("--unaff-max-expansion-prob-het must be between 0 and 1")
+    if args.unaff_min_expansion_prob_het is not None:
+        if args.unaff_min_expansion_prob_het < 0 or args.unaff_min_expansion_prob_het > 1:
+            common.ERROR("--unaff-min-expansion-prob-het must be between 0 and 1")
+    if args.unaff_min_expansion_prob_het is not None and args.unaff_max_expansion_prob_het is not None:
+        if args.unaff_min_expansion_prob_het > args.unaff_max_expansion_prob_het:
+            common.ERROR("--unaff-min-expansion-prob-het must be less than --unaff-max-expansion-prob-het")
+    if args.affec_max_expansion_prob_hom is not None:
+        if args.affec_max_expansion_prob_hom < 0 or args.affec_max_expansion_prob_hom > 1:
+            common.ERROR("--affec-max-expansion-prob-hom must be between 0 and 1")
+    if args.affec_min_expansion_prob_hom is not None:
+        if args.affec_min_expansion_prob_hom < 0 or args.affec_min_expansion_prob_hom > 1:
+            common.ERROR("--affec-min-expansion-prob-hom must be between 0 and 1")
+    if args.affec_min_expansion_prob_hom is not None and args.affec_max_expansion_prob_hom is not None:
+        if args.affec_min_expansion_prob_hom < args.affec_max_expansion_prob_hom:
+            common.ERROR("--affec-min-expansion-prob-hom must be less than --affec-max-expansion-prob-hom")
+    if args.unaff_max_expansion_prob_hom is not None:
+        if args.unaff_max_expansion_prob_hom < 0 or args.unaff_max_expansion_prob_hom > 1:
+            common.ERROR("--unaff-max-expansion-prob-hom must be between 0 and 1")
+    if args.unaff_min_expansion_prob_hom is not None:
+        if args.unaff_min_expansion_prob_hom < 0 or args.unaff_min_expansion_prob_hom > 1:
+            common.ERROR("--unaff-min-expansion-prob-hom must be between 0 and 1")
+    if args.unaff_min_expansion_prob_hom is not None and args.unaff_max_expansion_prob_hom is not None:
+        if args.unaff_min_expansion_prob_hom < args.unaff_max_expansion_prob_hom:
+            common.ERROR("--unaff-min-expansion-prob-hom must be less than --unaff-max-expansion-prob-hom")
+    if args.affec_max_expansion_prob_total is not None:
+        if args.affec_max_expansion_prob_total < 0 or args.affec_max_expansion_prob_total > 1:
+            common.ERROR("--affec-max-expansion-prob-total must be between 0 and 1")
+    if args.affec_min_expansion_prob_total is not None:
+        if args.affec_min_expansion_prob_total < 0 or args.affec_min_expansion_prob_total > 1:
+            common.ERROR("--affec-min-expansion-prob-total must be between 0 and 1")
+    if args.affec_min_expansion_prob_total is not None and args.affec_max_expansion_prob_total is not None:
+        if args.affec_min_expansion_prob_total < args.affec_max_expansion_prob_total:
+            common.ERROR("--affec-min-expansion-prob-total must be less than --affec-max-expansion-prob-total")
+    if args.unaff_max_expansion_prob_total is not None:
+        if args.unaff_max_expansion_prob_total < 0 or args.unaff_max_expansion_prob_total > 1:
+            common.ERROR("--unaff-max-expansion-prob-total must be between 0 and 1")
+    if args.unaff_min_expansion_prob_total is not None:
+        if args.unaff_min_expansion_prob_total < 0 or args.unaff_min_expansion_prob_total > 1:
+            common.ERROR("--unaff-min-expansion-prob-total must be between 0 and 1")
+    if args.unaff_min_expansion_prob_total is not None and args.unaff_max_expansion_prob_total is not None:
+        if args.unaff_min_expansion_prob_total < args.unaff_max_expansion_prob_total:
+            common.ERROR("--unaff-min-expansion-prob-total must be less than --unaff-max-expansion-prob-total")
 
 def GetAllCallFilters():
     """
@@ -161,18 +197,18 @@ def BuildPostMaSTRCallFilters(args):
     - cdict ({'affec': list<filters.Filter>, 'unaff': list<filters.Filter>}): list of call-level filters
     """
     cdict = {'affec':[], 'unaff':[]}
-    if args.affec_expansion_prob_het is not None:
-        cdict['affec'].append(filters.ProbHet(args.affec_expansion_prob_het))
-    if args.affec_expansion_prob_hom is not None:
-        cdict['affec'].append(filters.ProbHom(args.affec_expansion_prob_hom))
-    if args.affec_expansion_prob_total is not None:
-        cdict['affec'].append(filters.ProbTotal(args.affec_expansion_prob_total))
-    if args.unaff_expansion_prob_het is not None:
-        cdict['unaff'].append(filters.ProbHet(args.unaff_expansion_prob_het))
-    if args.unaff_expansion_prob_hom is not None:
-        cdict['unaff'].append(filters.ProbHom(args.unaff_expansion_prob_hom))
-    if args.unaff_expansion_prob_total is not None:
-        cdict['unaff'].append(filters.ProbTotal(args.unaff_expansion_prob_total))
+    if args.affec_min_expansion_prob_het is not None:
+        cdict['affec'].append(filters.ProbHet(args.affec_min_expansion_prob_het))
+    if args.affec_min_expansion_prob_hom is not None:
+        cdict['affec'].append(filters.ProbHom(args.affec_min_expansion_prob_hom))
+    if args.affec_min_expansion_prob_total is not None:
+        cdict['affec'].append(filters.ProbTotal(args.affec_min_expansion_prob_total))
+    if args.unaff_min_expansion_prob_het is not None:
+        cdict['unaff'].append(filters.ProbHet(args.unaff_min_expansion_prob_het))
+    if args.unaff_min_expansion_prob_hom is not None:
+        cdict['unaff'].append(filters.ProbHom(args.unaff_min_expansion_prob_hom))
+    if args.unaff_min_expansion_prob_total is not None:
+        cdict['unaff'].append(filters.ProbTotal(args.unaff_min_expansion_prob_total))
     return cdict
 
 
@@ -204,15 +240,21 @@ def main():
     
     #### Affected sample filters
     affec_group = parser.add_argument_group("Call-level filters specific to affected samples")
-    affec_group.add_argument("--affec-expansion-prob-het", help="Expansion prob-value threshold. Filters calls with probability of heterozygous expansion less than this", type=float)
-    affec_group.add_argument("--affec-expansion-prob-hom", help="Expansion prob-value threshold. Filters calls with probability of homozygous expansion less than this", type=float)
-    affec_group.add_argument("--affec-expansion-prob-total", help="Expansion prob-value threshold. Filters calls with probability of total expansion less than this", type=float)
+    affec_group.add_argument("--affec-max-expansion-prob-het", help="Expansion prob-value threshold. Filters calls with probability of heterozygous expansion less than this", type=float)
+    affec_group.add_argument("--affec-min-expansion-prob-het", help="Expansion prob-value threshold. Filters calls with probability of heterozygous expansion more than this", type=float)
+    affec_group.add_argument("--affec-max-expansion-prob-hom", help="Expansion prob-value threshold. Filters calls with probability of homozygous expansion less than this", type=float)
+    affec_group.add_argument("--affec-min-expansion-prob-hom", help="Expansion prob-value threshold. Filters calls with probability of homozygous expansion more than this", type=float)
+    affec_group.add_argument("--affec-max-expansion-prob-total", help="Expansion prob-value threshold. Filters calls with probability of total expansion less than this", type=float)
+    affec_group.add_argument("--affec-min-expansion-prob-total", help="Expansion prob-value threshold. Filters calls with probability of total expansion more than this", type=float)
     
     #### Unaffected sample filters
     unaff_group = parser.add_argument_group("Call-level filters specific to unaffected samples")
-    unaff_group.add_argument("--unaff-expansion-prob-het", help="Expansion prob-value threshold. Filters calls with probability of heterozygous expansion less than this", type=float)
-    unaff_group.add_argument("--unaff-expansion-prob-hom", help="Expansion prob-value threshold. Filters calls with probability of homozygous expansion less than this", type=float)
-    unaff_group.add_argument("--unaff-expansion-prob-total", help="Expansion prob-value threshold. Filters calls with probability of total expansion less than this", type=float)
+    unaff_group.add_argument("--unaff-max-expansion-prob-het", help="Expansion prob-value threshold. Filters calls with probability of heterozygous expansion less than this", type=float)
+    unaff_group.add_argument("--unaff-min-expansion-prob-het", help="Expansion prob-value threshold. Filters calls with probability of heterozygous expansion more than this", type=float)
+    unaff_group.add_argument("--unaff-max-expansion-prob-hom", help="Expansion prob-value threshold. Filters calls with probability of homozygous expansion less than this", type=float)
+    unaff_group.add_argument("--unaff-min-expansion-prob-hom", help="Expansion prob-value threshold. Filters calls with probability of homozygous expansion more than this", type=float)
+    unaff_group.add_argument("--unaff-max-expansion-prob-total", help="Expansion prob-value threshold. Filters calls with probability of total expansion less than this", type=float)
+    unaff_group.add_argument("--unaff-min-expansion-prob-total", help="Expansion prob-value threshold. Filters calls with probability of total expansion more than this", type=float)
 
     debug_group = parser.add_argument_group("Debugging parameters")
     debug_group.add_argument("--num-records", help="Only process this many records", type=int)
