9ec29eab662025ff9517113eb2be06803a76e62b
ileenamitra <ileenamitra@gmail.com>
2019-05-14 14:31:06 -0700

    added HWE p and obs het output in statSTR

--- statSTR/statSTR.py
+++ statSTR/statSTR.py
@@ -4,7 +4,7 @@
 Tool for computing stats on STR VCF files
 """
 # Example: statSTR --vcf /storage/mgymrek/gangstr-analysis/round2/trio/trio_gangstr_filtered_level1.vcf.gz --out test.tab --thresh
-# Example: statSTR --vcf 
+# Example: statSTR --vcf
 # TODO add arguments for other stats here: mean, heterozygosity
 
 # Imports
@@ -12,6 +12,14 @@ import argparse
 import os
 import sys
 import vcf
+
+# Load local libraries
+import dumpSTR.filters as filters
+# import strtools.utils.common as common
+# import strtools.utils.utils as utils
+sys.path.append("$HOME/STRtools/utils/")
+import common
+import utils
 
 """
 Compute the maximum allele length seen
@@ -41,7 +49,15 @@ def GetAFreq(record, samplelist=[]):
     if total == 0: return "."
     afreqs = ",".join(["%s:%.3f"%(a, acounts.get(a, 0)*1.0/total) for a in sorted(acounts.keys())])
     return afreqs
-            
+
+def GetHWEP(record, samplelist=[], use_length=False, het_output=False):
+    '''For each STR loci, it outputs the Hardy-Weinberg equilibrium exact test p-value.
+    If het=True, then Extended output is HWE P and obs_het.
+    If het=False, then Standard output is HWE P.'''
+    return utils.GetSTRHWE(record, samples=samplelist, uselength=use_length, het_output=het_output)
+
+
+
 def main():
     parser = argparse.ArgumentParser(__doc__)
     inout_group = parser.add_argument_group("Input/output")
@@ -53,6 +69,10 @@ def main():
     stat_group = parser.add_argument_group("Stats group")
     stat_group.add_argument("--thresh", help="Output threshold field (for GangSTR strinfo). Threshold is set to the max observed allele length", action="store_true")
     stat_group.add_argument("--afreq", help="Output allele frequencies", action="store_true")
+    stat_group.add_argument("--hwep", help="Output HWE p-values per loci.", action="store_true")
+    stat_group.add_argument("--het", help="Output observed heterozygote counts used for HWE per loci.", action="store_true")
+    stat_group.add_argument("--use-length", help="Calculate per-locus stats (het, HWE) collapsing alleles by length", action="store_true")
+
     args = parser.parse_args()
 
     # Load samples
@@ -64,11 +84,13 @@ def main():
     header = ["chrom","start","end"]
     if args.thresh: header.append("thresh")
     if args.afreq: header.append("afreq")
+    if args.hwep: header.append("hwep")
+    if args.het: header.append("obs_het")
     if args.out == "stdout":
         outf = sys.stdout
     else:
         outf = open(args.out, "w")
-        outf.write("\t".join(header)+"\n")
+    outf.write("\t".join(header)+"\n")
 
     if args.region: regions = invcf.fetch(args.region)
     else: regions = invcf
@@ -78,6 +100,12 @@ def main():
             items.append(GetThresh(record, samplelist=samplelist))
         if args.afreq:
             items.append(GetAFreq(record, samplelist=samplelist))
+        if args.hwep & ~args.het:
+            items.append(GetHWEP(record, samplelist=samplelist, use_length=args.use_length, het_output=args.het))
+        if args.hwep & args.het:
+            items.append(GetHWEP(record, samplelist=samplelist, use_length=args.use_length, het_output=args.het)[0])
+        if args.het:
+            items.append(GetHWEP(record, samplelist=samplelist, use_length=args.use_length, het_output=args.het)[1])
         outf.write("\t".join([str(item) for item in items])+"\n")
 
     outf.close()
