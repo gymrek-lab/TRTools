8683070765ada6c5459afa9acafdef7670260c6a
gymreklab <melissa.gymrek@gmail.com>
2020-02-17 14:14:22 -0800

    adding function to infer formats to stratify

--- compareSTR/compareSTR.py
+++ compareSTR/compareSTR.py
@@ -23,6 +23,47 @@ else: # pragma: no cover
     import strtools.utils.tr_harmonizer as trh # pragma: no cover
     import strtools.utils.utils as utils  # pragma: no cover
 
+def GetFormatFields(format_fields, format_binsizes, format_fileoption, vcfreaders):
+    r"""Get which FORMAT fields to stratify on
+
+    Also perform some checking on user arguments
+
+    Parameters
+    ----------
+    format_fields : str
+        Comma-separated list of FORMAT fields to stratify on
+    format_binsizes: str
+        Comma-separated list of min:max:binsize, one for each FORMAT field.
+    format_fileoption : {0, 1, 2}
+        Whether each format field needs to be in both readers (0), reader 1 (1) or reader 2 (2)
+    vcfreaders : list of vcf.Reader
+        List of readers. Needed to check if required FORMAT fields are present
+
+    Returns
+    -------
+    formats : list of str
+        List of FORMAT fields to stratify on
+    binsizes : list of float
+        List of binsizes for each FORMAT field
+    """
+    if format_fields is None or format_binsizes is None:
+        return [], []
+    formats = format_fields.split(",")
+    binsizes = format_binsizes.split(",")
+    if len(formats) != len(binsizes):
+        raise ValueError("--stratify-formats must be same length as --stratify-binsizes")
+    binsizes = [[float(x) for x in item.split(":")] for item in binsizes]
+    for fmt in formats:
+        check1 = (fmt in vcfreaders[0].formats)
+        check2 = (fmt in vcfreaders[1].formats)
+        if format_fileoption == 0 and not (check1 and check2):
+            raise ValueError("FORMAT field %s must be present in both VCFs if --stratify-file=0"%fmt)
+        if format_fileoption == 1 and not check1:
+            raise ValueError("FORMAT field %s must be present in --vcf1 if --stratify-file=1"%fmt)
+        if format_fileoption == 2 and not check2:
+            raise ValueError("FORMAT field %s must be present in --vcf2 if --stratify-file=2"%fmt)
+    return formats, binsizes
+
 def getargs():  # pragma: no cover
     parser = argparse.ArgumentParser(__doc__)
     ### Required arguments ###
@@ -38,11 +79,14 @@ def getargs():  # pragma: no cover
     ### Stratify results ###
     stats_group = parser.add_argument_group("Metrics to stratify results")
     stats_group.add_argument("--stratify-fields", help="Comma-separated list of FORMAT fields to stratify by", type=str)
-    stats_group.add_argument("--stratify-binsizes", help="Comma-separated list of binsizes to stratify each field on. Must be same length as --stratify-fields.", type=str)
+    stats_group.add_argument("--stratify-binsizes", help="Comma-separated list of min:max:binsize to stratify each field on. Must be same length as --stratify-fields.", type=str)
     stats_group.add_argument("--stratify-file", help="Set to 1 to stratify based on --vcf1. Set to 2 to stratify based on --vcf2. Set to 0 to apply stratification to both --vcf1 and --vcf2", default=0, type=int)
     stats_group.add_argument("--period", help="Report results overall and also stratified by repeat unit length (period)", action="store_true")
+    ### Optional args ###
+    option_group = parser.add_argument_group("Optional arguments")
+    option_group.add_argument("--verbose", help="Print helpful debugging info", action="store_true")
     args = parser.parse_args()
-    return ags
+    return args
 
 def main(args):
     ### Check and load VCF files ###
@@ -55,15 +99,15 @@ def main(args):
 
     ### Walk through sorted readers, merging records as we go ###
     current_records = [next(reader) for reader in vcfreaders]
-    is_min = GetMinRecords(current_records, chroms)
-    done = DoneReading(current_records)
+    is_min = mergeutils.GetMinRecords(current_records, chroms)
+    done = mergeutils.DoneReading(current_records)
 
     ### Load shared samples ###
-    samples = mergeutils.GetSharedSamples(readers)
+    samples = mergeutils.GetSharedSamples(vcfreaders)
+    print(samples)
 
     ### Determine FORMAT fields we should look for ###
-    # TODO
-    format_fields = []
+    format_fields, format_binsizes = GetFormatFields(args.stratify_fields, args.stratify_binsizes, args.stratify_file, vcfreaders)
 
     ### Keep track of data to summarize at the end ###
     results_dir = {
