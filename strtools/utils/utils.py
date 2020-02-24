22ec9a6da028c8243e4506f1b31539ee9023dbc2
mgymrek <melissa.gymrek@gmail.com>
2020-02-23 21:43:29 -0800

    fixing occasional round error in validate allele freqws

--- strtools/utils/utils.py
+++ strtools/utils/utils.py
@@ -32,7 +32,7 @@ def ValidateAlleleFreqs(allele_freqs):
     True
     """
     if len(allele_freqs.keys()) == 0: return False
-    return sum(allele_freqs.values()) == 1
+    return abs(1-sum(allele_freqs.values())) <= 0.001
 
 def GetHeterozygosity(allele_freqs):
     r"""Compute heterozygosity of a locus
