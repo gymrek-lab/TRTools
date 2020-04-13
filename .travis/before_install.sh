42f1c9f265ec66bb61a6e471e35c030bc87594e0
Nima Mousavi <mousavi@ucsd.edu>
2020-04-13 15:30:02 -0700

    changed brew update line

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -1,8 +1,9 @@
 set -e
 set -v
 
-if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then brew update; fi
+#if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then brew update; fi
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then 
+  brew update;
   brew install python3;
   virtualenv venv -p python3;
   source venv/bin/activate;
