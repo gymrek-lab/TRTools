5296f49a86f48759bbaac74e3a5723be1677e8dc
Nima Mousavi <mousavi@ucsd.edu>
2020-04-23 14:37:55 -0700

    updated version.py and removed clutter

--- trtools/utils/version.py
+++ trtools/utils/version.py
@@ -1,8 +1,5 @@
 """
 Version number
 """
-import os
-#version_file = open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..", "_version"))
-#__version__ = version_file.read().strip()
 __version__ = '2.0.4'
 
