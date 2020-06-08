52dfcae245a761971d90149426f6684dcfcb4d6a
Jonathan Margoliash <jmargolpeople@gmail.com>
2020-06-08 12:45:47 -0700

    Add notes about PopSTR quality scores, clean up docs so they stop throwing errors during Sphinx build

--- doc/conf.py
+++ doc/conf.py
@@ -87,4 +87,5 @@ html_theme_options = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# We don't use this, so comment it out so that it doesn't generate a warning
+# html_static_path = ['_static']
