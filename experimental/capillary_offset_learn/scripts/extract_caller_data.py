98ffb42e71486d46ed68ae156a16c3bc6d2ba5fc
Nima Mousavi <mousavi@tscc-login11.sdsc.edu>
2021-08-12 16:58:06 -0700

    added gangstr and hipstr calls

---
+++ experimental/capillary_offset_learn/scripts/extract_caller_data.py
@@ -0,0 +1,137 @@
+import pandas as pd
+import shlex, json
+import subprocess
+methods = ['hipstr'] # ['gangstr' 'eh', 'hipstr', 'advntr']
+suffix = {'gangstr': '_merged.vcf.gz',
+        'hipstr': '.vcf.gz',
+        'advntr': '_merged.vcf.gz',
+        }
+outs_root = '/projects/ps-gymreklab/mousavi/results/1000genomes/'
+
+
+# Load product sizes to get list of samples and primer/locus ids
+col_list = ["PrimerID", "SampleID"]
+ps_df = pd.read_csv("../data/csv/1000g_product_sizes.csv", usecols=col_list)
+samples_list = list(set(ps_df['SampleID']))
+samples_list.remove('Reference')
+samples_list.remove('reference')
+# print(samples_list)
+locus_id_list = list(set(ps_df['PrimerID']))
+# print(locus_id_list)
+
+# Load list of loci to get chrom, start, end
+col_list = ["LocusID", "Chrom", "Start", "End", "Motif"]
+lc_df = pd.read_csv("../data/csv/1000g_loci.csv", usecols=col_list)
+def get_chrom_start_end_motif(lid):
+    q_df = lc_df.query('LocusID == @lid')
+    if len(q_df) == 0:
+        return None, None, None, None
+    return list(q_df['Chrom'])[0], list(q_df['Start'])[0], list(q_df['End'])[0], list(q_df['Motif'])[0]
+
+# Load pop and spop for each sample
+sm_df = pd.read_csv('/projects/ps-gymreklab/mousavi/analysis/1000genomes/samples/all_samples.txt', sep='\t', names = ['SampleID', 'SubPop', 'SuperPop', 'Sex', 'CramPath'])
+def get_spop_pop(sid):
+    q_df = sm_df.query('SampleID == @sid')
+    if len(q_df) == 0:
+        return None, None
+    return list(q_df['SuperPop'])[0], list(q_df['SubPop'])[0]
+
+def convert_data_to_dict(proc_line, meth, motif):
+    if meth == 'gangstr':
+        return {'cn': proc_line[0], 
+            'ci': proc_line[1], 
+            'rc': proc_line[2],
+            'q': proc_line[3]}
+    elif meth == 'hipstr':
+        if proc_line[2] == '.':
+            return {}
+        ref = proc_line[0]
+        alts = proc_line[1].split(',')
+        if '|' in proc_line[2]:
+            gt = proc_line[2].split('|')
+        elif '/' in proc_line[2]:
+            gt = proc_line[2].split('/')
+        else:
+            raise ValueError('Invalid GT: ', proc_line[2])
+        q = proc_line[3]
+        alleles = []
+        allele_lens = []
+        cn = []
+        for a in gt:
+            if int(a) == 0:
+                alleles.append(ref)
+                allele_lens.append(str(len(ref)))
+                cn.append(str(len(ref) / len(motif)))
+            else:
+                alleles.append(alts[int(a) - 1])
+                allele_lens.append(str(len(alts[int(a) - 1])))
+                cn.append(str(len(alts[int(a) - 1]) / len(motif)))
+        return {'alleles': ','.join(alleles), 
+            'allele_lens': ','.join(allele_lens), 
+            'cn': ','.join(cn),
+            'q': q}
+    return {}
+
+
+
+
+# create a dictionary for output
+out_data = {}
+for meth in methods:
+    out_data[meth] = {}
+
+
+
+for meth in methods:
+    meth_outs = outs_root + meth + '_outs/'
+    for lid in locus_id_list:
+        out_data[meth][lid] = {}
+        chrom, start, end, motif = get_chrom_start_end_motif(lid)
+        if chrom is None: 
+            continue
+        # if lid != 'CACNA1A':
+        #     continue
+        print(lid + '\t' + chrom + ':' + str(start) + '-' + str(end))
+
+        # iterate over all samples
+        for sample in samples_list:
+            spop, pop = get_spop_pop(sample)
+            if spop is None:
+                continue
+            if pop != 'eh':
+                vcf_dir = meth_outs + '/'.join([spop, pop, 'merged', pop + suffix[meth]])
+            else:
+                vcf_dir = meth_outs + '/'.join(['eh-polymorphic--1kg-all-hg38','merged', pop + '_merged.vcf.gz'])
+            proc_line = -1
+            if meth == 'gangstr':
+                p = subprocess.Popen(shlex.split("bcftools query {vcf} -r {ch}:{st} -s {samp} -f '[%REPCN@%REPCI@%RC@%Q]\n'"\
+                    .format(vcf=vcf_dir, ch=chrom, st=start, samp=sample)),
+                    stdout=subprocess.PIPE)
+                p.wait()
+                out_lines = p.stdout.readline()
+                if out_lines is None:
+                    continue
+                if len(out_lines) == 0:
+                    continue
+                proc_line = out_lines.strip().decode("utf-8").split('@')
+            elif meth == 'hipstr':
+                p = subprocess.Popen(shlex.split("bcftools query {vcf} -r {ch}:{st} -s {samp} -f '[%REF@%ALT@[%GT@%Q]]\n'"\
+                    .format(vcf=vcf_dir, ch=chrom, st=start, samp=sample)),
+                    stdout=subprocess.PIPE)
+                # print("bcftools query {vcf} -r {ch}:{st} -s {samp} -f '[%REF|%ALT|[%Q]]\n'"\
+                #     .format(vcf=vcf_dir, ch=chrom, st=start, samp=sample))
+                p.wait()
+                out_lines = p.stdout.readline()
+                if out_lines is None:
+                    continue
+                if len(out_lines) == 0:
+                    continue
+                proc_line = out_lines.strip().decode("utf-8").split('@')
+            if sample not in out_data[meth][lid]:
+                if proc_line != -1:
+                    out_data[meth][lid][sample] = convert_data_to_dict(proc_line, meth, motif)
+            else:
+                raise ValueError('Duplicate sample:', sample)
+    with open('../data/json/' + meth  + '_calls.json', 'w') as outjson:
+        json.dump(out_data[meth], outjson, indent=4)
+print(out_data)
