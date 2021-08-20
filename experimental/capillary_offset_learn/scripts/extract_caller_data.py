1ede88212d6fc0fa7bd64278984576ee30a1d7a4
Nima Mousavi <mousavi@tscc-login1.sdsc.edu>
2021-08-20 14:58:49 -0700

    added eh and advntr dara

--- experimental/capillary_offset_learn/scripts/extract_caller_data.py
+++ experimental/capillary_offset_learn/scripts/extract_caller_data.py
@@ -1,12 +1,12 @@
 import pandas as pd
 import shlex, json
 import subprocess
-methods = ['gangstr', 'hipstr', 'advntr'] # eh
+methods = ['advntr'] # eh
 suffix = {'gangstr': '_merged.vcf.gz',
         'hipstr': '.vcf.gz',
         'advntr': '_merged.vcf.gz',
         }
-dir_root = "/gymreklab-tscc" # /projects/ps-gymreklab
+dir_root = "/projects/ps-gymreklab" #"/gymreklab-tscc" # 
 outs_root = '/%s/mousavi/results/1000genomes/'%dir_root
 
 
@@ -41,11 +41,10 @@ def convert_data_to_dict(proc_line, meth, motif):
     if meth == 'gangstr':
         ref = proc_line[0]
         ref_len = len(ref)
-        m_len = len(motif)
 
         gb = []
         for c in proc_line[1].split(','):
-            gb.append(str(int(c) * m_len - ref_len))
+            gb.append(str(int(c) * len(motif) - ref_len))
         return {'cn': proc_line[1], 
             'ci': proc_line[2], 
             'gb': ','.join(gb),
@@ -82,6 +81,47 @@ def convert_data_to_dict(proc_line, meth, motif):
             'gb': ','.join(gb),
             'cn': ','.join(cn),
             'q': q}
+    elif meth == 'eh':
+        ref_len = int(proc_line[0]) * len(motif)
+        gb = []
+        for c in proc_line[1].split('/'):
+            gb.append(str(int(c) * len(motif) - ref_len))
+        return {'cn': proc_line[1], 
+            'ci': proc_line[2], 
+            'gb': ','.join(gb)
+            }
+    elif meth == 'advntr':
+        if proc_line[2] == '.':
+            return {}
+        ref = proc_line[0]
+        alts = proc_line[1].split(',')
+        if '|' in proc_line[2]:
+            gt = proc_line[2].split('|')
+        elif '/' in proc_line[2]:
+            gt = proc_line[2].split('/')
+        else:
+            raise ValueError('Invalid GT: ', proc_line[2])
+
+        alleles = []
+        allele_lens = []
+        cn = []
+        for a in gt:
+            if int(a) == 0:
+                alleles.append(ref)
+                allele_lens.append(str(len(ref)))
+                cn.append(str(len(ref) / len(motif)))
+            else:
+                alleles.append(alts[int(a) - 1])
+                allele_lens.append(str(len(alts[int(a) - 1])))
+                cn.append(str(len(alts[int(a) - 1]) / len(motif)))
+        gb = []
+        for c in cn:
+            gb.append(str(float(c) * len(motif) - len(ref)))
+
+        return {'alleles': ','.join(alleles), 
+            'allele_lens': ','.join(allele_lens),
+            'gb': ','.join(gb),
+            'cn': ','.join(cn)}
     return {}
 
 
@@ -110,10 +150,11 @@ for meth in methods:
             spop, pop = get_spop_pop(sample)
             if spop is None:
                 continue
-            if pop != 'eh':
+            if meth != 'eh':
                 vcf_dir = meth_outs + '/'.join([spop, pop, 'merged', pop + suffix[meth]])
             else:
                 vcf_dir = meth_outs + '/'.join(['eh-polymorphic--1kg-all-hg38','merged', pop + '_merged.vcf.gz'])
+
             proc_line = -1
             if meth == 'gangstr':
                 p = subprocess.Popen(shlex.split("bcftools query {vcf} -r {ch}:{st} -s {samp} -f '[%REF@%REPCN@%REPCI@%RC@%Q]\n'"\
@@ -139,6 +180,30 @@ for meth in methods:
                 if len(out_lines) == 0:
                     continue
                 proc_line = out_lines.strip().decode("utf-8").split('@')
+            elif meth == 'eh':
+                p = subprocess.Popen(shlex.split("bcftools query {vcf} -r {ch}:{st} -s {samp} -f '[%INFO/REF@%REPCN@%REPCI]\n'"\
+                    .format(vcf=vcf_dir, ch=chrom, st=start, samp=sample)),
+                    stdout=subprocess.PIPE)
+                p.wait()
+                out_lines = p.stdout.readline()
+                if out_lines is None:
+                    continue
+                if len(out_lines) == 0:
+                    continue
+                proc_line = out_lines.strip().decode("utf-8").split('@')
+            elif meth == 'advntr':
+                p = subprocess.Popen(shlex.split("bcftools query {vcf} -r {ch}:{st} -s {samp} -f '[%REF@%ALT@[%GT]]\n'"\
+                    .format(vcf=vcf_dir, ch=chrom, st=start, samp=sample)),
+                    stdout=subprocess.PIPE)
+                # print("bcftools query {vcf} -r {ch}:{st} -s {samp} -f '[%REF|%ALT|[%Q]]\n'"\
+                #     .format(vcf=vcf_dir, ch=chrom, st=start, samp=sample))
+                p.wait()
+                out_lines = p.stdout.readline()
+                if out_lines is None:
+                    continue
+                if len(out_lines) == 0:
+                    continue
+                proc_line = out_lines.strip().decode("utf-8").split('@')
             if sample not in out_data[meth][lid]:
                 if proc_line != -1:
                     out_data[meth][lid][sample] = convert_data_to_dict(proc_line, meth, motif)
@@ -147,3 +212,4 @@ for meth in methods:
     with open('../data/json/' + meth  + '_calls.json', 'w') as outjson:
         json.dump(out_data[meth], outjson, indent=4)
 print(out_data)
+
