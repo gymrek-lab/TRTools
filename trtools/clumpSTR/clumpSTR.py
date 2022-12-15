a9791c2f60534f5433b41d3a5952e0fe9c443b1f
Melissa Gymrek <melissagymrek@Melissas-MacBook-Pro.local>
2022-12-15 09:36:23 -0800

    adding clumpstr arguments

--- trtools/clumpSTR/clumpSTR.py
+++ trtools/clumpSTR/clumpSTR.py
@@ -17,6 +17,13 @@ def getargs():  # pragma: no cover
         __doc__,
         formatter_class=utils.ArgumentDefaultsHelpFormatter
     )
+    ### Input genotypes and summ stats ###
+    input_group = parser.add_argument_group("Inputs")
+    input_group.add_argument("--summstats-snps", help="SNP summary statistics", type=str)
+    input_group.add_argument("--summstats-strs", help="STR summary statistics", type=str)
+    input_group.add_argument("--gts-snps", help="SNP genotypes", type=str)
+    input_group.add_argument("--gts-strs", help="STR genotypes (VCF format)", type=str)
+    ### Clumping arguments ###
     ### Optional args ###
     ver_group = parser.add_argument_group("Version")
     ver_group.add_argument("--version", action="version", version='{version}'.format(version=__version__))
@@ -24,7 +31,7 @@ def getargs():  # pragma: no cover
     return args
 
 def main(args):
-	pass # TODO - do all of the things
+    pass # TODO - do all of the things
 
 def run():  # pragma: no cover
     args = getargs()
