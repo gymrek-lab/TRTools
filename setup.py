72c8c6528de718d792586fc201820dd8f6176d65
Jonathan Margoliash <jmargolpeople@gmail.com>
2023-01-31 17:10:56 -0800

    Bump version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ LICENSE = 'MIT'
 curdir = os.path.abspath(os.path.dirname(__file__))
 MAJ = 5
 MIN = 0
-REV = 0
+REV = 1
 VERSION = '%d.%d.%d' % (MAJ, MIN, REV)
 with open(os.path.join(curdir, 'trtools/version.py'), 'w') as fout:
         fout.write(
