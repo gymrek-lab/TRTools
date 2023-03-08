f8dad5bcfc7ad5ada165f79f10323cdaabe41f31
aarushi03 <suzy20159@gmail.com>
2023-03-07 16:40:13 -0800

    Changed the theme of doc page for TRTools

--- doc/conf.py
+++ doc/conf.py
@@ -64,7 +64,7 @@ autodoc_typehints = 'description'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 html_sidebars = {
     '**' : [
