42c638f14d37ff0b75a69c2956d4ec93d441bee3
gymreklab <melissa.gymrek@gmail.com>
2023-07-03 12:43:33 -0700

    adding initial test file for mosaicSTR

---
+++ trtools/mosaicSTR/tests/test_mosaicSTR.py
@@ -0,0 +1,34 @@
+import argparse
+import os
+
+import pytest
+
+from ..mosaicSTR import *
+
+# Set up base argparser
+@pytest.fixture
+def args(tmpdir):
+    args = argparse.ArgumentParser()
+    args.vcf = None
+    args.out = str(tmpdir / "test")
+    args.region = None
+    args.only_passing = False
+    args.debug = False
+    args.vcftype = "hipstr"
+    return args
+
+# Test no such file or directory
+def test_WrongFile(args, vcfdir):
+    fname = os.path.join(vcfdir, "test_non_existent.vcf")
+    if os.path.exists(fname):
+        os.remove(fname)
+    args.vcf = fname
+    retcode = main(args)
+    assert retcode==1
+
+# Test the right file or directory
+def test_RightFile(args, vcfdir):
+    fname = os.path.join(vcfdir, "test_hipstr.vcf")
+    args.vcf = fname
+    retcode = main(args)
+    assert retcode==0
