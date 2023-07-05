b10e065ed9a213cfb8911b5a71f4507d52e0f91d
gymreklab <melissa.gymrek@gmail.com>
2023-07-05 15:46:24 -0700

    adding error checking to simtr

--- trtools/simTR/simTR.py
+++ trtools/simTR/simTR.py
@@ -10,9 +10,56 @@ import shutil
 import sys
 from pyfaidx import Fasta
 import trtools.utils.utils as utils
+from trtools import __version__
 
 def main(args):
-	pass # TODO
+	if not os.path.exists(args.coords):
+		common.WARNING("Error: {} does not exist".format(args.coords))
+		return 1
+	if not os.path.exists(args.ref):
+		common.WARNING("Error: {} does not exist".format(args.ref))
+		return 1
+	if args.art is not None:
+		if not os.path.exists(args.art):
+			common.WARNING("Error: ART path {} does not exist".format(args.art))
+			return 1
+	else:
+		if shutil.which("art_illumina") is None:
+			common.WARNING("Error: Could not find art_illumina executable")
+			return 1
+	if args.u < 0 or args.u > 1:
+		common.WARNING("Error: --u u ({}) is not between 0 and 1".format(args.u))
+		return 1
+	if args.d < 0 or args.d > 1:
+		common.WARNING("Error: --d ({}) is not between 0 and 1".format(args.d))
+		return 1
+	if args.rho < 0 or args.rho > 1:
+		common.WARNING("Error: --rho ({}}) is not between 0 and 1".format(args.rho))
+		return 1
+	if args.p_thresh < 0 or args.p_thresh > 1:
+		common.WARNING("Error: --p_thresh ({}) is not between 0 and 1".format(args.p_thresh))
+		return 1
+	if args.coverage < 0:
+		common.WARNING("Error: --coverage ({}}) cannot be less than 0".format(args.coverage))
+		return 1
+	if args.read_length < 0:
+		common.WARNING("Error: --read_length ({}) cannot be less than 0".format(args.read_length))
+		return 1
+	if args.insert < 0:
+		common.WARNING("Error: --insert ({}) cannot be less than 0".format(args.insert))
+		return 1
+	if args.sd < 0:
+		common.WARNING("Error: --sd ({}) cannot be less than 0".format(args.sd))
+		return 1
+	if args.window < 0:
+		common.WARNING("Error: --window ({}) cannot be less than 0".format(args.window))
+		return 1
+	if not os.path.exists(os.path.dirname(os.path.abspath(args.output_dir))):
+		common.WARNING("Error: The directory which contains the output location {} does"
+			" not exist".format(args.output_dir))
+		return 1
+
+	# TODO
 
 def getargs():
 	parser = argparse.ArgumentParser(
