527488d2ce515dcaa4990b0b45eee64d7dd15e19
gymreklab <melissa.gymrek@gmail.com>
2023-07-06 17:19:47 -0700

    adding base simtr test argparse fixture

--- trtools/simTR/tests/test_simTR.py
+++ trtools/simTR/tests/test_simTR.py
@@ -5,4 +5,26 @@ import pytest
 
 from ..simTR import *
 
+# Set up base argparser
+@pytest.fixture
+def args(tmpdir):
+    args = argparse.ArgumentParser()
+    args.ref = None
+    args.repeat_unit = None
+    args.outprefix = None
+    args.tmpdir = None
+    args.u = None
+    args.d = None
+    args.rho = None
+    args.p_thresh = None
+    args.coverage = None
+    args.read_length = None
+    args.insert = None
+    args.sd = None
+    args.window = None
+    args.art = None
+    args.single = False
+    args.debug = False
+    return args
+
 # TODO add tests
