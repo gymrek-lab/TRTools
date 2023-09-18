6916c4f7339bee748e4fff521a580406d4dde94d
Aarushi Sehgal <suzy20159@gmail.com>
2023-09-18 11:00:34 -0700

    Updated __init__.py
    Replaced mosaicSTR references by prancSTR

--- trtools/prancSTR/__init__.py
+++ trtools/prancSTR/__init__.py
@@ -1,4 +1,4 @@
-# expose the code in the file mosaicSTR/mosaicSTR.py
-# through the statement import trtools.mosaicSTR
-# instead of through import trtools.mosaicSTR.mosaicSTR
-from .mosaicSTR import *
+# expose the code in the file prancSTR/prancSTR.py
+# through the statement import trtools.prancSTR
+# instead of through import trtools.prancSTR.prancSTR
+from .prancSTR import *
