d13bc4dae228075d573feacf039d3dc0f48ceb3a
gymreklab <melissa.gymrek@gmail.com>
2023-09-18 14:12:06 -0700

    removing mosaicSTR folder

--- trtools/mosaicSTR/__init__.py
+++
@@ -1,4 +0,0 @@
-# expose the code in the file mosaicSTR/mosaicSTR.py
-# through the statement import trtools.mosaicSTR
-# instead of through import trtools.mosaicSTR.mosaicSTR
-from .mosaicSTR import *
