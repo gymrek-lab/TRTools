6485243b4406e94ca8f617695c065a980bfab77b
Arya Massarat <23412689+aryarm@users.noreply.github.com>
2023-10-31 16:39:24 -0700

    infer version from git tags instead

--- trtools/__init__.py
+++ trtools/__init__.py
@@ -1,2 +1,10 @@
-from .version import __version__
+try:
+    from importlib.metadata import version, PackageNotFoundError
+except ImportError:
+    # handles py < 3.8, since importlib.metadata was introduced in py3.8
+    from importlib_metadata import version, PackageNotFoundError
 
+    try:
+        __version__ = version(__name__)
+    except PackageNotFoundError:
+        __version__ = "unknown"
