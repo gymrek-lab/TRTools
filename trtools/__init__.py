2a42c413a22c152abdfefce3c2c4650d94badce0
Arya Massarat <23412689+aryarm@users.noreply.github.com>
2023-10-31 16:47:44 -0700

    generate version.py file at installation

--- trtools/__init__.py
+++ trtools/__init__.py
@@ -1,10 +1,1 @@
-try:
-    from importlib.metadata import version, PackageNotFoundError
-except ImportError:
-    # handles py < 3.8, since importlib.metadata was introduced in py3.8
-    from importlib_metadata import version, PackageNotFoundError
-
-    try:
-        __version__ = version(__name__)
-    except PackageNotFoundError:
-        __version__ = "unknown"
+from .version import __version__
