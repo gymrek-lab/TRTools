eaf24f7fbd93e9ee3bde3da287953c8788980f35
Arya Massarat <23412689+aryarm@users.noreply.github.com>
2023-12-24 22:09:54 +0000

    try running nox once

---
+++ noxfile.py
@@ -0,0 +1,66 @@
+"""Nox sessions."""
+import os
+import shutil
+from pathlib import Path
+
+import nox
+from nox_poetry import Session
+from nox_poetry import session
+
+
+package = "trtools"
+python_versions = ["3.7", "3.8", "3.9", "3.10", "3.11"]
+nox.needs_version = ">= 2022.11.21"
+nox.options.sessions = (
+    "tests",
+)
+
+
+def install_handle_python_numpy(session):
+    """
+    handle incompatibilities with python and numpy versions
+    see https://github.com/cjolowicz/nox-poetry/issues/1116
+    """
+    if session._session.python == "3.11":
+        session._session.install(".")
+    else:
+        session.install(".")
+
+
+# detect whether conda/mamba is installed
+if os.getenv("CONDA_EXE"):
+    conda_cmd = "conda"
+    if (Path(os.getenv("CONDA_EXE")).parent / "mamba").exists():
+        conda_cmd = "mamba"
+    conda_args = ["-c", "conda-forge"]
+
+    @session(venv_backend=conda_cmd, venv_params=conda_args, python=python_versions)
+    def tests(session: Session) -> None:
+        """Run the test suite."""
+        session.conda_install(
+            "numpy",
+            "pytest",
+            channel="conda-forge",
+        )
+        session.conda_install(
+            "art",
+            "bcftools",
+            channel="bioconda",
+        )
+        install_handle_python_numpy(session)
+        session.run(
+           "pytest", "--doctest-modules", *session.posargs
+        )
+        session.run("./test/cmdline_tests.sh")
+
+else:
+
+    @session(python=python_versions)
+    def tests(session: Session) -> None:
+        """Run the test suite."""
+        session.install("pytest")
+        install_handle_python_numpy(session)
+        session.run(
+            "pytest", "--doctest-modules", *session.posargs
+        )
+        session.run("./test/cmdline_tests.sh")
