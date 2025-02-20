6bdc596db02dba1e38f7ab11fba8cb20377e0eaf
Arya Massarat <23412689+aryarm@users.noreply.github.com>
2025-02-20 09:27:36 -0800

    docs: drop support for py 3.8 and update to poetry 2 in our CI (#249)
    Co-authored-by: Aarushi Sehgal <suzy20159@gmail.com>

--- noxfile.py
+++ noxfile.py
@@ -8,7 +8,7 @@ from nox_poetry import session
 
 
 package = "trtools"
-python_versions = ["3.7", "3.8", "3.9", "3.10", "3.11", "3.12"]
+python_versions = ["3.7", "3.8", "3.9", "3.10", "3.11", "3.12", "3.13"]
 nox.needs_version = ">= 2022.11.21"
 nox.options.sessions = (
     "tests",
@@ -19,20 +19,6 @@ cov_cli_args = [
     "--cov=.",
     "--cov-report=term-missing",
 ]
-
-
-def install_handle_python(session):
-    """
-    handle incompatibilities between python and other packages
-    see https://github.com/cjolowicz/nox-poetry/issues/1116
-    """
-    # install the latest versions of all dependencies for py3.9+
-    # but install the locked versions for < py3.9 to ensure some stability in the CI and
-    # help us understand when we need to bump our version constraints
-    if session._session.python in ("3.9", "3.10", "3.11", "3.12"):
-        session._session.install(".")
-    else:
-        session.install(".")
 
 
 # detect whether conda/mamba is installed
@@ -56,7 +42,7 @@ if os.getenv("CONDA_EXE"):
             "pytest-cov",
             channel="conda-forge",
         )
-        install_handle_python(session)
+        session.install(".")
         session.run(
            "python", "-m", "pytest", *cov_cli_args, *session.posargs
         )
@@ -68,7 +54,7 @@ else:
     def tests(session: Session) -> None:
         """Run the test suite."""
         session.install("pytest", "pytest-cov")
-        install_handle_python_numpy(session)
+        session.install(".")
         session.run(
             "python", "-m", "pytest", *cov_cli_args, *session.posargs
         )
