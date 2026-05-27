35c10d1d408ce25cdefc625402e88417eff46e03
nicholema <nicholema621@gmail.com>
2026-05-27 17:10:32 +0000

    made change to annotatr so it can run a small region instead of the whole chrom

--- trtools/annotaTR/annotaTR.py
+++ trtools/annotaTR/annotaTR.py
@@ -607,10 +607,16 @@ def main(args):
     # If using a ref panel, assume we have same number
     # of TRs as the panel
     # Otherwise, assume our file is all TRs and use record count
-    if refpanel_metadata is not None:
-        variant_ct = ref_variant_ct
-    else:
-        variant_ct = reader.num_records
+    #if refpanel_metadata is not None:
+    #    variant_ct = ref_variant_ct
+    #else:
+    #    variant_ct = reader.num_records
+
+    # Use actual variant count from input VCF
+    variant_ct = reader.num_records
+    #if refpanel_metadata is not None:
+        # Use ref panel count as upper bound only
+    #    variant_ct = ref_variant_ct
     if OutputFileTypes.pgen in outtypes:
         pgen_writer, pvar_writer = GetPGenPvarWriter(reader, args.out, variant_ct)
 
